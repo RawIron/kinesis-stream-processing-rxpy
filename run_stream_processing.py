76c6bc030bc1199dcd92b5f9a8d3b0f6335f60f6
RawIron <manianology@gmail.com>
2016-08-26 11:00:28 +0200

    script allows to run the stream processing locally. imports consumer and producer. runs each in a different process and connect the processes with a queue. uses dependency injection to configure cosumer and producer.

---
+++ run_stream_processing.py
@@ -0,0 +1,44 @@
+import multiprocessing as mp
+import inject
+import Queue as StdQueue
+import time
+
+import producer
+import consumer
+
+
+# Injector
+#
+def pipeline(binder):
+  binder.install(producer.file_base)
+  binder.install(consumer.queue_base)
+
+  q = mp.Queue()
+  binder.bind(mp.Queue, q)
+
+  binder.bind(producer.EventQueue, q)
+  binder.bind(producer.EventWriter, producer.QueueEventWriter(q))
+
+  binder.bind(consumer.Stream, consumer.infinite_mpqueue_stream(q))
+
+
+inject.configure(pipeline)
+
+
+# Stream Processing
+#
+def worker(q):
+  consumer.consume()
+
+
+q = inject.instance(mp.Queue)
+
+p = mp.Process(target=worker, args=(q,))
+p.start()
+
+producer.produce()
+
+q.close()
+q.join_thread()
+
+p.join()
